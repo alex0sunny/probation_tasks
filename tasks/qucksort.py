46e9d9460eec2c378313d46c22d2b1b6dbc75a44
alex0sunny <30532471+alex0sunny@users.noreply.github.com>
2020-12-10 22:22:36 +0300

    Update qucksort.py

--- tasks/qucksort.py
+++ tasks/qucksort.py
@@ -31,3 +31,20 @@ def f(nums):
   f(nums[i:])
 
 f(ar)
+
+def quicksort(nums, fst, lst):
+  if fst >= lst: return
+
+  i, j = fst, lst
+  pivot = nums[lst]
+
+  while i <= j:
+    while nums[i] < pivot:
+      i += 1
+    while nums[j] > pivot:
+      j -= 1
+    if i <= j:
+      nums[i], nums[j] = nums[j], nums[i]
+      i, j = i + 1, j - 1
+  quicksort(nums, fst, j)
+  quicksort(nums, i, lst)
