aaaa24e66b32c53cba9b8e4014575b49f06a1554
alex0sunny <30532471+alex0sunny@users.noreply.github.com>
2020-12-14 15:40:13 +0300

    Create qucksort_no_recursion.py

---
+++ tasks/qucksort_no_recursion.py
@@ -0,0 +1,22 @@
+def quicksort(ar, ranges = None): 
+  if ranges == None:
+    ranges = [[0, len(ar) - 1]]
+  while ranges != []:
+    [start, end] = ranges[0]
+    ranges = ranges[1:]
+    if start >= end:
+      continue
+    pivot = ar[end]
+    left = start
+    right = end
+    pivot = ar[end]
+    while left < right:
+      while ar[left] < pivot:
+        left += 1
+      while ar[right] > pivot:
+        right -= 1
+      if left < right:
+        ar[left], ar[right] = ar[right], ar[left]
+        if ar[left] == ar[right]:
+          left += 1
+    ranges = [[start, left - 1], [left + 1, end]] + ranges
