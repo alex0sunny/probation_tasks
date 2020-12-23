bd67dc2c97caa75b9187a3f104594aa1315c5827
alex0sunny <30532471+alex0sunny@users.noreply.github.com>
2020-12-23 16:55:39 +0300

    add description

--- tasks/area_under_curves.clj
+++ tasks/area_under_curves.clj
@@ -1,3 +1,20 @@
+;; Definite Integrals via Numerical Methods
+;; This relates to definite integration via numerical methods.
+;; Consider the algebraic expression given by:
+;; a1 * x^b1 + a2 * x^b2 + a3 * x^b3 ... an * x^bn
+;; For the purpose of numerical computation, the area under the curve between the limits and
+;; can be computed by the Limit Definition of a Definite Integral.
+;; Here is some background about areas and volume computation.
+
+;; Using equal subintervals of length = 0.001, you need to:
+;;     Evaluate the area bounded by a given polynomial function of the kind described above,
+;; between the given limits of L and R.
+;; and
+;;     Evaluate the volume of the solid obtained by revolving this polynomial curve around the
+;; x-axis.
+
+;; A relative error margin of 0.01 will be tolerated.
+
 (def read-input #(read-string (str "(" (read-line) ")")))
 (def a-list (read-input))
 (def b-list (read-input))
