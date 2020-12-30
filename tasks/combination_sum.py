5324e6ee7782e5e5174d6b525c9f234aadad3376
alex0sunny <30532471+alex0sunny@users.noreply.github.com>
2020-12-31 00:04:00 +0300

    Update combination_sum.py

--- tasks/combination_sum.py
+++ tasks/combination_sum.py
@@ -1,8 +1,15 @@
+# Given a collection of candidate numbers (candidates) and a target number (target),
+# find all unique combinations in candidates where the candidate numbers sum to target.
+
+# Each number in candidates may only be used once in the combination.
+
+# Note: The solution set must not contain duplicate combinations.
+
 def combination_sum(candidates, target):
     nums = list(sorted(candidates))
     res = []
 
-    def f(start, target, path=[]):
+    def f(start, target, path):
         if target == 0:
             res.append(path)
             return
