f9af65b740a12b41bb3aefe74a44a3221cbab58b
alex0sunny <30532471+alex0sunny@users.noreply.github.com>
2021-06-30 11:51:55 +0300

    Update cutting_metal_surplus.py

--- tasks/cutting_metal_surplus.py
+++ tasks/cutting_metal_surplus.py
@@ -26,10 +26,11 @@
 # 1230
 
 def maxProfit(costPerCut, salePrice, lengths):
-    price = 0
-    for cur_len in range(1, max(lenghts) + 1):
-        n_of_rods = sum([l // cur_len for l in lengths])
-        n_of_cuts = sum([max(0, l // cur_len - int(l % cur_len == 0)) for l in lengths])
-        price = max(price, n_of_rods * cur_len * salePrice - n_of_cuts * costPerCut)
-        cur_len -= 1
-    return price
+        price = 0
+        for cur_len in range(1, max(lengths) + 1):
+            n_of_rods = sum([l // cur_len for l in lengths])
+            n_of_cuts = sum([max(0, l // cur_len - int(l % cur_len == 0)) for l in lengths])
+            price = max(price, n_of_rods * cur_len * salePrice - n_of_cuts * costPerCut)
+            cur_len -= 1
+        return price
+
