9e08f31225603a9b94ed82ed801b68f712de4cc6
alex0sunny <30532471+alex0sunny@users.noreply.github.com>
2023-01-02 16:51:49 +0300

    restored interview problem
    restored the problem from the interview from memory

---
+++ tasks/remove_smiles.py
@@ -0,0 +1,31 @@
+'''
+Смайликом считается последовательность, начинающаяся с символа : или ; , далее могут следовать символы -
+и заканчивается скобками ()[]{}. Никакие другие символы не могут участвовать в смайлике.
+Задача: удалить смайлики из строки.
+'''
+
+
+def remove_smiles(s):
+    print('in:', s)
+    pos = 0
+    out = []
+    while pos < len(s):
+        if s[pos] not in ';:':
+            out.append(s[pos])
+            pos += 1
+            continue
+        back = pos
+        pos += 1
+        while pos < len(s) and s[pos] == '-':
+            pos += 1
+        while pos < len(s) and s[pos] in '(){}[]':
+            pos += 1
+        if s[pos-1] not in '(){}[]':
+            out.extend(s[back:pos])
+    print('out:', ''.join(out))
+    return ''.join(out)
+
+
+assert remove_smiles(';--)') == ''
+assert remove_smiles(';) abc::-----()()() a') == ' abc: a'
+assert remove_smiles('') == ''
