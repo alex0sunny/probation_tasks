ad05891b16a94685c88cdd58eda42974753e312d
alex0sunny <30532471+alex0sunny@users.noreply.github.com>
2023-01-10 22:47:38 +0300

    add another implementation

--- tasks/remove_comments_list.py
+++ tasks/remove_comments_list.py
@@ -1,4 +1,4 @@
-def remove_comments(source):
+def remove_comments_v0(source):
     block = False
     res = []
     for line in source:
@@ -17,3 +17,22 @@ def remove_comments(source):
         if line_out != '' and block == False:
             res.append(line_out)
     return res
+
+def remove_comments(self, lines):
+    s = '\n'.join(lines)
+    pos = 0
+    out = []
+    while pos < len(s):
+        if s[pos: pos+2] not in ['//', '/*']:
+            out.append(s[pos])
+            pos += 1
+            continue
+        pos += 2
+        if s[pos-1] == '/':
+            while pos < len(s) and s[pos] != '\n':
+                pos += 1
+            continue
+        while pos < len(s) and s[pos: pos+2] != '*/':
+            pos += 1
+        pos += 2
+    return [line for line in ''.join(out).split('\n') if line]
